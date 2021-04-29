41e08579d7fc51d4ba97fad7652f1dfc29366cae
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2021-04-29 08:39:14 -0400

    STY: formatting with black and minor corrections

--- pyspecfile.py
+++ pyspecfile.py
@@ -8,7 +8,7 @@ import time
 import itertools
 import numpy as np
 
-TIME_FORMAT = '%a %b %d %H:%M:%S %Y'
+TIME_FORMAT = "%a %b %d %H:%M:%S %Y"
 
 
 def split_sequence(iterable, size):
@@ -52,9 +52,9 @@ class SPECFileWriter(object):
 
     COLUMNS = 8
 
-    def __init__(self, filename, starting_number=None, comment='', motors=[]):
+    def __init__(self, filename, starting_number=None, comment="", motors=[]):
         if not filename:
-            raise SPECFileError('Must specify filename')
+            raise SPECFileError("Must specify filename")
 
         self.filename = os.path.abspath(filename)
         self._motors = list(sorted(motors))
@@ -68,7 +68,7 @@ class SPECFileWriter(object):
 
         if os.path.exists(filename) and os.path.getsize(filename) > 1:
             if not check_motor_list(filename, motors):
-                raise SPECFileMotorListError('Motor list does not match')
+                raise SPECFileMotorListError("Motor list does not match")
 
             with SPECFileReader(filename) as reader:
                 self.start_time = reader.epoch()
@@ -79,7 +79,7 @@ class SPECFileWriter(object):
                     self.scan_number = starting_number
 
             print('SPECFileWriter: Appending to "%s"' % filename)
-            self._f = open(filename, 'at')
+            self._f = open(filename, "at")
             self._fix_ending_newlines(filename)
 
             self._header_written = True
@@ -89,7 +89,7 @@ class SPECFileWriter(object):
                 starting_number = 0
 
             self.scan_number = starting_number
-            self._f = open(filename, 'wt')
+            self._f = open(filename, "wt")
             self._header_written = False
 
     def _get_motors(self):
@@ -98,8 +98,7 @@ class SPECFileWriter(object):
     def _set_motors(self, motors):
         if self._header_written:
             if motors != self._motors:
-                raise SPECFileMotorListError(
-                    'Cannot change motor list when header is already written')
+                raise SPECFileMotorListError("Cannot change motor list when header is already written")
             return
 
         self._motors = list(motors)
@@ -112,13 +111,13 @@ class SPECFileWriter(object):
         for i in range(amount - count):
             self.blank_line()
 
-    def _check_end_lines(self, filename, ending=2, char='\n'):
+    def _check_end_lines(self, filename, ending=2, char="\n"):
         try:
-            f = open(filename, 'rt')
+            f = open(filename, "rt")
             # Seek two bytes before the end of the file
             f.seek(-ending, 2)
             return f.read().count(char)
-        except:
+        except Exception:
             return 0
 
     def close(self):
@@ -127,7 +126,7 @@ class SPECFileWriter(object):
             self._f = None
 
     def write_line(self, line):
-        line = '%s\n' % line
+        line = "%s\n" % line
         if self._buffer_write:
             self._buffer.append(line)
         else:
@@ -138,9 +137,9 @@ class SPECFileWriter(object):
             self._f.write(line)
 
     def write_info(self, tag, data):
-        self.write_line('#%s %s' % (tag, data))
+        self.write_line("#%s %s" % (tag, data))
 
-    def write_scan_start(self, number=None, command='', scan_info=None, seconds=None):
+    def write_scan_start(self, number=None, command="", scan_info=None, seconds=None):
         """
         Writes the following:
         Scan information, date, wait time
@@ -164,11 +163,11 @@ class SPECFileWriter(object):
         self._buffer_write = True
 
         # S - scan number and command
-        self.write_info('S', '%d  %s' % (number, command))
+        self.write_info("S", "%d  %s" % (number, command))
         self.write_date()
         if seconds is not None:
             # T - scan/settling time
-            self.write_info('T', '%f (Seconds)' % (seconds, ))
+            self.write_info("T", "%f (Seconds)" % (seconds,))
 
     def write_scan_data_start(self, columns):
         if columns is None:
@@ -177,9 +176,9 @@ class SPECFileWriter(object):
         self._data_lines = 0
 
         # N - Number of columns
-        self.write_info('N', len(columns))
+        self.write_info("N", len(columns))
         # L - Column names (double-space separated)
-        self.write_info('L', '  '.join(columns))
+        self.write_info("L", "  ".join(columns))
 
     def write_motor_positions(self, positions):
         if not positions:
@@ -188,13 +187,13 @@ class SPECFileWriter(object):
         for i, pos in enumerate(split_sequence(positions, self.COLUMNS)):
             # P{num} - starting motor positions (that aren't necessarily being
             # scanned)
-            tag = 'P%d' % i
-            self.write_info(tag, ' '.join([str(p) for p in pos]))
+            tag = "P%d" % i
+            self.write_info(tag, " ".join([str(p) for p in pos]))
 
     def write_scan_data(self, data):
         self._buffer_write = False
         self._data_lines += 1
-        self.write_line(' '.join([str(d) for d in data]))
+        self.write_line(" ".join([str(d) for d in data]))
 
     def write_mca_calib(self, a, b, c):
         """
@@ -202,18 +201,17 @@ class SPECFileWriter(object):
 
         CALIBRATION = a + b*CHANNEL + c*CHANNEL^2
         """
-        self.write_line('#@CALIB  %.7g %.7g %.7g' % (a, b, c))
+        self.write_line("#@CALIB  %.7g %.7g %.7g" % (a, b, c))
 
     def write_mca_data(self, data, calibration=None, first=False):
-        """
-        """
+        """ """
         if data is None or np.size(data) == 0:
             return
 
         self._buffer_write = False
         self._data_lines += 1
         # does it really support multiple sets of MCA data per scan?
-        #if isinstance(data, (list, tuple)):
+        # if isinstance(data, (list, tuple)):
         #    if isinstance(data[0], np.ndarray):
         #        for array_ in data:
         #            self.write_mca_data(array_, calibration=calibration, first=first)
@@ -221,15 +219,15 @@ class SPECFileWriter(object):
 
         if first:
             # MCA format (full line)
-            self.write_line('#@MCA %%%dC' % len(data))
+            self.write_line("#@MCA %%%dC" % len(data))
 
             # number of channels, first idx, last idx, reduction coefficient
-            self.write_line('#@CHANN  %d 0 %d 1' % (len(data), len(data) - 1))
+            self.write_line("#@CHANN  %d 0 %d 1" % (len(data), len(data) - 1))
 
             if calibration:
                 self.write_mca_calib(*calibration)
 
-        self.write_line('@A %s' % ' '.join([str(d) for d in data]))
+        self.write_line("@A %s" % " ".join([str(d) for d in data]))
 
     @property
     def date_string(self):
@@ -239,13 +237,13 @@ class SPECFileWriter(object):
 
     def _write_starting_seconds(self):
         self.start_time = time.time()
-        self.write_info('E', int(self.start_time))
+        self.write_info("E", int(self.start_time))
 
     def write_date(self):
-        self.write_info('D', self.date_string)
+        self.write_info("D", self.date_string)
 
     def write_timestamp(self):
-        self.write_info('E', self.date_string)
+        self.write_info("E", self.date_string)
 
     def write_scan_aborted(self, points_written=None):
         self._buffer_write = False
@@ -254,8 +252,7 @@ class SPECFileWriter(object):
             self._data_lines = points_written
 
         if self._data_lines > 0:
-            self.write_info('C', '%s.  Scan aborted after %d points.' %
-                           (self.date_string, self._data_lines))
+            self.write_info("C", "%s.  Scan aborted after %d points." % (self.date_string, self._data_lines))
             self.blank_line()
             self._data_lines = 0
             self._f.flush()
@@ -265,21 +262,21 @@ class SPECFileWriter(object):
             self._buffer = []
 
     def blank_line(self):
-        self.write_line('')
+        self.write_line("")
 
-    def _write_header(self, comment='', motors=[]):
+    def _write_header(self, comment="", motors=[]):
         self._header_written = True
-        self.write_info('F', self.filename)
+        self.write_info("F", self.filename)
         self._write_starting_seconds()
         self.write_date()
         if comment is not None:
-            for line in comment.split('\n'):
-                self.write_info('C', line)  # TODO multiple lines valid?
+            for line in comment.split("\n"):
+                self.write_info("C", line)  # TODO multiple lines valid?
 
         for i, mot in enumerate(split_sequence(motors, self.COLUMNS)):
             # O{num} - motor names (that aren't necessarily being scanned)
-            tag = 'O%d' % i
-            self.write_info(tag, '  '.join([str(m) for m in mot]))
+            tag = "O%d" % i
+            self.write_info(tag, "  ".join([str(m) for m in mot]))
 
         self.blank_line()
         self._data_lines = 0
@@ -299,9 +296,9 @@ class SPECFileReader(object):
 
     def __init__(self, filename, parse_data=True):
         if not os.path.exists(filename):
-            raise ValueError('Invalid SPEC filename')
+            raise ValueError("Invalid SPEC filename")
 
-        self._f = open(filename, 'rt')
+        self._f = open(filename, "rt")
         self._in_scan = False
         self._buffer_lines = []
         self._buffer_groups = []
@@ -314,7 +311,7 @@ class SPECFileReader(object):
 
         self.spec_filename = filename
         self.motors = []
-        self.comment = ''
+        self.comment = ""
 
         self._read_header()
 
@@ -336,13 +333,13 @@ class SPECFileReader(object):
                 yield line
 
             line = self._f.readline()
-            if line == '':
-                yield '#S'  # TODO: fix so this isn't necessary
+            if line == "":
+                yield "#S"  # TODO: fix so this isn't necessary
                 self._eof = True
                 break
 
             line = line.strip()
-            if line and (line.startswith('#') or self._in_scan):
+            if line and (line.startswith("#") or self._in_scan):
                 yield line
 
     def _read_group(self):
@@ -360,16 +357,16 @@ class SPECFileReader(object):
 
                 break
 
-            if line.startswith('#'):
+            if line.startswith("#"):
                 # print('-> %s' % line)
-                if ' ' in line[1:]:
-                    tag, info = line[1:].split(' ', 1)
+                if " " in line[1:]:
+                    tag, info = line[1:].split(" ", 1)
                 else:
-                    tag, info = line[1], ''
+                    tag, info = line[1], ""
 
                 info = info.lstrip()
 
-                m = re.match('([@a-zA-Z]+)(\d*)', tag)
+                m = re.match("([@a-zA-Z]+)(\d*)", tag)
                 if m:
                     tag, tag_index = m.groups()
                 else:
@@ -393,7 +390,7 @@ class SPECFileReader(object):
                 if self._parse_data:
                     self._parse_scan_line(line)
                 else:
-                    self._scan['unparsed'].append(line)
+                    self._scan["unparsed"].append(line)
 
             if tag_done:
                 yield current_tag.upper(), group
@@ -403,8 +400,8 @@ class SPECFileReader(object):
                 tag_done = False
 
     def _parse_list(self, list_):
-        list_ = re.sub('\s+', ',', list_)
-        return list_.split(',')
+        list_ = re.sub("\s+", ",", list_)
+        return list_.split(",")
 
     def _parse_header_F(self, spec_filename):
         self.spec_filename = spec_filename
@@ -430,13 +427,13 @@ class SPECFileReader(object):
                     break
 
             # print('section', section, tag, lines)
-            lines = '  '.join(lines)
-            fcn_name = '_parse_%s_list_%s' % (section, tag)
+            lines = "  ".join(lines)
+            fcn_name = "_parse_%s_list_%s" % (section, tag)
             if hasattr(self, fcn_name):
                 fcn = getattr(self, fcn_name)
                 fcn(self._parse_list(lines))
 
-            fcn_name = '_parse_%s_%s' % (section, tag)
+            fcn_name = "_parse_%s_%s" % (section, tag)
             if hasattr(self, fcn_name):
                 # print('calling', fcn_name)
                 fcn = getattr(self, fcn_name)
@@ -444,84 +441,84 @@ class SPECFileReader(object):
 
             first_tag = False
 
-    _read_header = lambda self: self._read_section('header', end_tags=['S'])
+    def _read_header(self):
+        self._read_section("header", end_tags=["S"])
 
     def _parse_scan_S(self, scan_info):
-        number, command = scan_info.split(' ', 1)
+        number, command = scan_info.split(" ", 1)
 
         self._in_scan = True
         self._scan = {
-            'lines': [],
-            'mca_data': [],
-            'columns': [],
-            'time': 0,
-            'hkl': [0, 0, 0],
-            'fourc': [],
-            'positions': [],
+            "lines": [],
+            "mca_data": [],
+            "columns": [],
+            "time": 0,
+            "hkl": [0, 0, 0],
+            "fourc": [],
+            "positions": [],
         }
 
         if not self._parse_data:
-            self._scan['unparsed'] = []
+            self._scan["unparsed"] = []
 
-        self._scan['number'] = number
-        self._scan['command'] = command.strip()
+        self._scan["number"] = number
+        self._scan["command"] = command.strip()
 
         self._scans.append(self._scan)
 
     def _parse_scan_list_L(self, columns):
-        self._scan['columns'] = columns
+        self._scan["columns"] = columns
 
     def _parse_scan_D(self, date_):
-        self._scan['time'] = time.mktime(time.strptime(date_, TIME_FORMAT))
+        self._scan["time"] = time.mktime(time.strptime(date_, TIME_FORMAT))
 
     def _parse_scan_list_G(self, fourc_info):
-        self._scan['fourc'] = fourc_info
+        self._scan["fourc"] = fourc_info
 
     def _parse_scan_list_Q(self, hkl):
-        self._scan['hkl'] = hkl
+        self._scan["hkl"] = hkl
 
     def _parse_scan_line(self, line):
-        if line.startswith('@A'):
-            line = line.split(' ', 1)[1]
+        if line.startswith("@A"):
+            line = line.split(" ", 1)[1]
 
             # Line is now everything after @A
-            data = [float(f) for f in line.split(' ')]
+            data = [float(f) for f in line.split(" ")]
 
             # If the MCA data is spread on multiple lines, extend the previous
             # data, otherwise it's a new set
             if self._mca_line:
-                self._scan['mca_data'][-1].extend(data)
+                self._scan["mca_data"][-1].extend(data)
             else:
-                self._scan['mca_data'].append(data)
+                self._scan["mca_data"].append(data)
 
-            self._mca_line = line.endswith('\\')
+            self._mca_line = line.endswith("\\")
         else:
             try:
-                line = line.replace('None', '0.0')  # TODO during saving
-                self._scan['lines'].append([float(f) for f in line.split(' ')])
+                line = line.replace("None", "0.0")  # TODO during saving
+                self._scan["lines"].append([float(f) for f in line.split(" ")])
             except Exception as ex:
-                print('Bad scan line: %s' % line)
+                print("Bad scan line: %s" % line)
 
     def parse_data(self, scan):
-        '''
+        """
         Parse the scan data after the file is loaded, if parse_data was set
-        '''
+        """
         if self._parse_data:
             return
 
-        if 'unparsed' not in scan:
+        if "unparsed" not in scan:
             return
 
         self._scan = scan
-        for line in scan['unparsed']:
+        for line in scan["unparsed"]:
             self._parse_scan_line(line)
 
-        del scan['unparsed']
+        del scan["unparsed"]
 
     def _parse_scan_list_P(self, positions):
-        positions = [float(p) if p != 'None' else 0.0
-                     for p in positions]  # TODO fix
-        self._scan['positions'] = dict(zip(self.motors, positions))
+        positions = [float(p) if p != "None" else 0.0 for p in positions]  # TODO fix
+        self._scan["positions"] = dict(zip(self.motors, positions))
 
     @property
     def scans(self):
@@ -536,7 +533,13 @@ class SPECFileReader(object):
         if self._eof:
             return None
 
-        self._read_section('scan', end_tags=['S', ], ignore_first_tag=True)
+        self._read_section(
+            "scan",
+            end_tags=[
+                "S",
+            ],
+            ignore_first_tag=True,
+        )
         # should 'C' really be an end_tag, since you can use scan_on?
         # what does scan_on output look like?
         self._in_scan = False
@@ -553,9 +556,9 @@ class SPECFileReader(object):
 def check_motor_list(filename, motors):
     try:
         sf = SPECFileReader(filename)
-    except:
+    except Exception:
         return False
-    return (motors == sf.motors)
+    return motors == sf.motors
 
 
 def get_last_scan_number(filename):
@@ -568,22 +571,22 @@ def get_last_scan_number(filename):
 
         for scan in reader.scans:
             try:
-                numbers.append(int(scan['number']))
-            except:
+                numbers.append(int(scan["number"]))
+            except Exception:
                 pass
 
         return max(numbers)
 
     except Exception as ex:
-        print('Failed to get last scan number: (%s) %s' % (filename, ex))
+        print("Failed to get last scan number: (%s) %s" % (filename, ex))
         return 0
 
 
-if __name__ == '__main__':
-    motors = ['m0', 'm1']
-    data_names = ['test', 'one', 'two']
-    a = SPECFileWriter('test.txt', comment='my comment', motors=motors)
-    a.write_scan_start(command='dscan something', seconds=1)
+if __name__ == "__main__":
+    motors = ["m0", "m1"]
+    data_names = ["test", "one", "two"]
+    a = SPECFileWriter("test.txt", comment="my comment", motors=motors)
+    a.write_scan_start(command="dscan something", seconds=1)
     a.write_motor_positions([3, 4])
     a.write_scan_data_start(data_names)
     for d1, d2, d3 in zip(range(10), range(2, 12), range(10, 20)):
@@ -591,12 +594,12 @@ if __name__ == '__main__':
     a.finish_scan()
 
     # reader = SPECFileReader('/epics/data/aug_21_11')
-    reader = SPECFileReader('../test_output')
+    reader = SPECFileReader("../test_output")
     for scan in reader.scans:
-        print(scan['number'], scan['command'], scan.keys(), scan['columns'])
+        print(scan["number"], scan["command"], scan.keys(), scan["columns"])
 
     print(reader._scans[-1], len(reader._scans))
     print(reader._buffer_groups)
-    print(reader._scans[-1]['lines'])
-    print(reader._scans[-1]['columns'])
-    print(reader._scan['columns'])
+    print(reader._scans[-1]["lines"])
+    print(reader._scans[-1]["columns"])
+    print(reader._scan["columns"])
